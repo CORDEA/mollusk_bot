7fe2dbf5c3c9fb997a966883239b1e638f763a67
Yoshihiro Tanaka <contact@cordea.jp>
2018-11-10 23:28:46 +0900

    add displayable milestone

---
+++ displayable_milestone.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+from github import Milestone
+
+NEXT_MILESTONE_DAYS = 7
+
+
+@dataclass
+class DisplayableMilestone:
+    milestone: Milestone
+
+    @property
+    def is_next(self) -> bool:
+        date = self.milestone.due_on
+        now = datetime.utcnow()
+        return (date - now).days <= NEXT_MILESTONE_DAYS
