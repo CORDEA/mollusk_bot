15eeefc64766a7e0a30af28ce276a3977a2a63c0
Yoshihiro Tanaka <contact@cordea.jp>
2018-11-11 00:58:02 +0900

    detect bug level

--- displayable_label.py
+++ displayable_label.py
@@ -1,8 +1,19 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from github import Label
+
+import settings
 
 
 @dataclass
 class DisplayableLabel:
     label: Label
+    bug_level: int = field(init=False)
+
+    def __post_init__(self):
+        name = self.label.name
+        levels = settings.LABEL_BUG_LEVELS
+        if name in levels:
+            self.bug_level = levels.index(self.label.name) + 1
+        else:
+            self.bug_level = 0
