004b955b8caba6f2fc44fcf9c1d10194dde16b28
Yoshihiro Tanaka <contact@cordea.jp>
2018-11-11 12:15:52 +0900

    fetch pulls

--- github_client.py
+++ github_client.py
@@ -1,9 +1,11 @@
-from github import Github, Milestone
+from github import Github
 
 from displayable_issue import DisplayableIssue
 from displayable_issues import DisplayableIssues
 from displayable_milestone import DisplayableMilestone
 from displayable_milestones import DisplayableMilestones
+from displayable_pull import DisplayablePull
+from displayable_pulls import DisplayablePulls
 
 
 class GitHubClient:
@@ -12,11 +14,13 @@ class GitHubClient:
         self.__repository = github.get_repo(organization + '/' + repository)
 
     def fetch_issues(self, milestone: DisplayableMilestone) -> DisplayableIssues:
-        issues = self.__repository.get_issues(milestone=milestone.milestone, state='open', sort='created')
+        issues = self.__repository.get_issues(
+            milestone=milestone.milestone, state='open', sort='created', direction='desc')
         return DisplayableIssues(map(lambda i: DisplayableIssue(i), issues))
 
-    def fetch_prs(self):
-        raise NotImplementedError()
+    def fetch_pulls(self) -> DisplayablePulls:
+        pulls = self.__repository.get_pulls(state='open', sort='created', direction='desc')
+        return DisplayablePulls(map(lambda p: DisplayablePull(p), pulls))
 
     def fetch_milestones(self) -> DisplayableMilestones:
         milestones = self.__repository.get_milestones(state='open')
