361479fcfc040beb371aec20fb46f431bc27cfa1
Yoshihiro Tanaka <contact@cordea.jp>
2018-11-12 08:26:59 +0900

    add special header

--- formatter.py
+++ formatter.py
@@ -3,19 +3,23 @@ from displayable_milestone import DisplayableMilestone
 from displayable_pulls import DisplayablePulls
 
 HEADER = 'Hi, current state is as follows.'
+SPECIAL_HEADER = 'All tasks are completed. See you again.'
 
 
 class Formatter:
 
-    def __init__(self, pulls: DisplayablePulls, milestone: DisplayableMilestone=None, issues: DisplayableIssues=None):
+    def __init__(self, pulls: DisplayablePulls, milestone: DisplayableMilestone = None,
+                 issues: DisplayableIssues = None):
         self.pulls = pulls
         self.milestone = milestone
         self.issues = issues
 
     def format(self):
         output = HEADER
+        if self.pulls is None and self.issues is None:
+            return SPECIAL_HEADER
         if self.pulls is not None:
             output += '\n\n' + self.pulls.for_output()
-        if self.milestone is None or self.issues is None:
+        if self.issues is None:
             return output
         return output + '\n\n' + self.milestone.for_output(True) + ' issues:\n\n' + self.issues.for_output()
