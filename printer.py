16e8fff55c69bb5883e6bee9bf0dec4236d61707
Yoshihiro Tanaka <contact@cordea.jp>
2018-11-11 14:28:58 +0900

    enhance printer

--- printer.py
+++ printer.py
@@ -1,12 +1,18 @@
 from displayable_issues import DisplayableIssues
 from displayable_milestone import DisplayableMilestone
+from displayable_pulls import DisplayablePulls
 
 
 class Printer:
 
-    def __init__(self):
+    def __init__(self, debug: bool):
+        self.debug = debug
         self.milestone = None
         self.issues = None
+        self.pulls = None
+
+    def set_pulls(self, pulls: DisplayablePulls):
+        self.pulls = pulls
 
     def set_milestone(self, milestone: DisplayableMilestone):
         self.milestone = milestone
@@ -14,7 +20,14 @@ class Printer:
     def set_issues(self, issues: DisplayableIssues):
         self.issues = issues
 
-    def print(self) -> str:
+    def print(self):
+        output = ''
+        if self.pulls is not None:
+            output += self.pulls.for_output()
         if self.milestone is None or self.issues is None:
-            return ''
-        return self.milestone.for_output() + ' issues:\n\n' + self.issues.for_output()
+            return output
+        if output:
+            output += '\n\n'
+        output + self.milestone.for_output() + ' issues:\n\n' + self.issues.for_output()
+        if self.debug:
+            print(output)
